928f898a65af73a089ad00d611ed48ac1444f25b
Anthony Veaudry <anthony@veaudry.pro>
2018-03-09 18:01:31 +0200

    update test for checking if object is an instance of Occupancy
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = chai.assert;
 
 describe('Occupancy', function() {
   it('occupancy object should exist', function(){
-    var roomsHotel = new Occupancy
-    assert.deepEqual(roomsHotel, {})
+    var occupancies = new Occupancy
+    assert.equal(occupancies instanceof Occupancy, true)
   })
 })
