d4b2b6497103f4b9797e6c681978da2cb7c7a4a3
Anthony Veaudry <anthony@veaudry.pro>
2018-03-09 17:34:47 +0200

    update Occupancy object existance test
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var assert = chai.assert;
 
 describe('Occupancy', function() {
   it('occupancy object should exist', function(){
+    var roomsHotel = new Occupancy
     assert.deepEqual(roomsHotel, {})
   })
 })
