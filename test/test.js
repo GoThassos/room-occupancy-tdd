78ddfbf8ccb80c6eaf472c04df230bb24637e990
Anthony Veaudry <anthony@veaudry.pro>
2018-03-12 14:52:21 +0200

    refactor tests so that the test data is as variables and not constants
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- test/test.js
+++ test/test.js
@@ -1,14 +1,32 @@
 var assert = chai.assert;
 
-var rooms = [
-  [6,2],
-  [7,0],
-  [8,0],
-  [6,3],
-  [2,10],
-  [8,1],
-  [8]
-]
+var testA = {
+  rooms: [
+    [6,2],
+    [7,0],
+    [8,0],
+    [6,3],
+    [2,10],
+    [8,1],
+    [8]
+  ],
+  cleanRooms: [
+    [6,2],
+    [7,0],
+    [8,0],
+    [6,3],
+    [2,10],
+    [8,1],
+    [8,0]
+  ],
+  biggestRoom: [2,10],
+  roomMostAdults: [8,1],
+  maxPeople: 12,
+  maxAdultsBiggestRoom: 2,
+  maxChildrenBiggestRoom: 10,
+  maxAdultsRoomMostAdults: 8,
+  maxChildrenRoomMostAdults: 1
+}
 
 describe('Occupancy', function() {
   it('occupancy object should exist', function(){
@@ -16,78 +34,72 @@ describe('Occupancy', function() {
     assert.equal(occupancies instanceof Occupancy, true)
   })
 
+  const currentTest = testA;
+  const occupancies = new Occupancy(currentTest.rooms)
+
   describe('#list', function(){
-    var occupancies = new Occupancy(rooms)
     it('returns the initial room occupancies (with children defined)', function(){
-      assert.deepEqual(occupancies.list, [[6,2],[7,0],[8,0],[6,3],[2,10],[8,1],[8,0]])
+      assert.deepEqual(occupancies.list, currentTest.cleanRooms)
     })
 
     describe('#_list()', function(){
       it('has private method that adds zero children to array if none defined', function(){
-        assert.deepEqual(occupancies._list(rooms), [[6,2],[7,0],[8,0],[6,3],[2,10],[8,1],[8,0]])
+        assert.deepEqual(occupancies._list(currentTest.rooms), currentTest.cleanRooms)
       })
     })
   })
 
   describe('#_biggestRoom()', function(){
-    var occupancies = new Occupancy(rooms)
     it('returns array of room with biggest occupancy', function(){
-      assert.deepEqual(occupancies._biggestRoom(), [2,10])
+      assert.deepEqual(occupancies._biggestRoom(), currentTest.biggestRoom)
     })
   })
 
   describe('#_mostAdultsWithMostChildrenRoom()', function(){
-    var occupancies = new Occupancy(rooms)
     it('returns array of room occupancy with most adults and most children', function(){
-      assert.deepEqual(occupancies._mostAdultsWithMostChildrenRoom(), [8,1])
+      assert.deepEqual(occupancies._mostAdultsWithMostChildrenRoom(), currentTest.roomMostAdults)
     })
   })
 
   describe('#_totalPeople', function(){
-    var occupancies = new Occupancy(rooms)
     it('returns the sum of people in a room', function(){
-      assert.equal(occupancies._totalPeople([2,10]), 12)
+      assert.equal(occupancies._totalPeople(currentTest.biggestRoom), currentTest.maxPeople)
     })
   })
 
   describe('#maxPeople', function(){
-    var occupancies = new Occupancy(rooms)
     it('returns the max amount of peaple a room can hold as an integer', function(){
-      assert.equal(occupancies.maxPeople, 12)
+      assert.equal(occupancies.maxPeople, currentTest.maxPeople)
     })
 
     describe('#_maxPeople()', function(){
       it('has private method that add values of biggest room array element', function(){
-        assert.equal(occupancies._maxPeople(), 12)
+        assert.equal(occupancies._maxPeople(), currentTest.maxPeople)
       })
     })
   })
 
   describe('#maxAdultsBiggestRoom', function(){
-    var occupancies = new Occupancy(rooms)
     it('return the number of adults of the largest room by occupancy as integer', function(){
-      assert.equal(occupancies.maxAdultsBiggestRoom, 2)
+      assert.equal(occupancies.maxAdultsBiggestRoom, currentTest.maxAdultsBiggestRoom)
     })
   })
 
   describe('#maxChildrenBiggestRoom', function(){
-    var occupancies = new Occupancy(rooms)
     it('return the number of children of the largest room by occupancy as integer', function(){
-      assert.equal(occupancies.maxChildrenBiggestRoom, 10)
+      assert.equal(occupancies.maxChildrenBiggestRoom, currentTest.maxChildrenBiggestRoom)
     })
   })
 
   describe('#maxAdultsRoomMostAdults', function(){
-    var occupancies = new Occupancy(rooms)
     it('return the number of adults of the room with most adults and most children as integer', function(){
-      assert.equal(occupancies.maxAdultsRoomMostAdults, 8)
+      assert.equal(occupancies.maxAdultsRoomMostAdults, currentTest.maxAdultsRoomMostAdults)
     })
   })
 
   describe('#maxChildrenRoomMostAdults', function(){
-    var occupancies = new Occupancy(rooms)
     it('return the number of children of the room with most adults and most children as integer', function(){
-      assert.equal(occupancies.maxChildrenRoomMostAdults, 1)
+      assert.equal(occupancies.maxChildrenRoomMostAdults, currentTest.maxChildrenRoomMostAdults)
     })
   })
 
